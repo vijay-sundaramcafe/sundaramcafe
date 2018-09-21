d803c4223f0705e81a6a24eadaa4c8fb94abd76c
vijay-sundaramcafe <vijaypadmanaban45@gmail.com>
2018-09-21 08:56:44 +0530

    First Chages

---
+++ src/app/routing/routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavmenuComponent } from '../navmenu/navmenu.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class RoutingModule { }
